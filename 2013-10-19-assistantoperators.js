39aa375e3079c8f74f36aba12f508b02a0f68ecd
OpenShift guest <louie.c.diaz@gmail.com>
2013-11-30 15:30:47 -0500

    updates

---
+++ 2013-10-19-assistantoperators.js
@@ -0,0 +1,15 @@
+Assignment Operators:
+
+An assignment operator assigns a value to its left operannd based on the value of its right operands.
+The basic assignment operator is equal (=), which assigns the value of its right operand to its left operand.
+That is, x = y assigns the value of y to x. The other assignments operators are shorthands for operations listed
+in the following table:
+
+Operator:               Meaning:
+x += y
+x -= y
+x *= y
+x /= y
+x %= y
+x <<= y
+x >>= y
